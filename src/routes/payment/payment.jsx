5f712eb45d93e366951feae8108d759101fe2ecc
lolo16cm <mojiking8888@gmail.com>
2022-09-01 09:44:19 -0400

    remove Elements

--- src/routes/payment/payment.jsx
+++ src/routes/payment/payment.jsx
@@ -10,7 +10,7 @@ const Payment = () => {
     return(
         // <Elements stripe={stripePromise}>
             <PaymentDetails />
-        // </Elements>
+        // </Elements>git 
             
         
     );
